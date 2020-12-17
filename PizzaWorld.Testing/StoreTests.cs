1115f255b3defd8226abcc2338c1761b9be08266
Nicholas Hall <nthall1914@gmail.com>
2020-12-17 14:10:18 -0500

    initial commit

---
+++ PizzaWorld.Testing/StoreTests.cs
@@ -0,0 +1,23 @@
+using PizzaWorld.Domain.Models;
+using Xunit;
+
+namespace PizzaWorld.Testing
+{
+    public class StoreTests
+    {
+        [Fact]
+        private void Test_StoreExists()
+        {
+            // arrange
+            var sut = new Store();  // inferenece
+            Store sut1 = new Store(); // memory allocation
+
+            // act
+            var actual = sut;
+            
+            // assert
+            Assert.IsType<Store>(actual);
+            Assert.NotNull(actual);
+        }
+    }
+}
---
+++ PizzaWorld.Testing/UserTests.cs
@@ -0,0 +1,23 @@
+using PizzaWorld.Domain.Models;
+using Xunit;
+
+namespace PizzaWorld.Testing
+{
+    public class UserTests
+    {
+        [Fact]
+        private void Test_UserExists()
+        {
+            // arrange
+            var sut = new User();  // inferenece
+            User sut1 = new User(); // memory allocation
+
+            // act
+            var actual = sut;
+            
+            // assert
+            Assert.IsType<User>(actual);
+            Assert.NotNull(actual);
+        }
+    }
+}
