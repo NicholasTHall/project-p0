d4a24a12553fb308d1a13d4cfd7ee632b2bb72ed
Nicholas Hall <nthall1914@gmail.com>
2020-12-17 17:27:23 -0500

    add serialization to solution

--- PizzaWorld.Domain/Models/Store.cs
+++ PizzaWorld.Domain/Models/Store.cs
@@ -1,7 +1,24 @@
+using System.Collections.Generic;
+
 namespace PizzaWorld.Domain.Models
 {
     public class Store
     {
-        
+        public List<Order> Orders {get; set;}
+        void CreateOrder()
+        {
+            Orders.Add(new Order());
+        }
+
+        bool DeleteOrder(Order ClientOrder)
+        {
+            try{
+                Orders.Remove(ClientOrder);
+
+                return true;
+            } catch {
+                return false;
+            }         
+        }
     }
 }
